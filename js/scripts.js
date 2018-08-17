bc0228a83120ac3e0f44f5751a9e1f692ea9f251
Chris Crow <chrismcrow@gmail.com>
2018-08-17 10:20:25 -0700

    pass 1st & 2nd specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 //business logic
-
+function beepFunction(input) {
+  if (input.includes("0")) {
+    return "<em>Beep!</em>";
+  } else {
+    number = parseInt(input)
+    console.log("else " + number)
+    return number;
+  }
+};
 
 //user interface logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var numberInput = $("#numberInput").val();
-    $(".numberOutput").text(beepFunction(numberInput) + "<br>");
+    $(".numberOutput").html(beepFunction(numberInput) + "<br>");
   });
 });
