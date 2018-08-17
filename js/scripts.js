c3d2da9060ae13fc0e2b7c436ab2c83458630d48
Chris Crow <chrismcrow@gmail.com>
2018-08-17 10:45:50 -0700

    pass 7th spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,29 @@
 //business logic
 function beepFunction(input) {
-  if (parseInt(input) % 3 === 0) {
-    return "<em>I'm sorry, Dave. I'm afraid I can't do that.</em>"
-  } else if (input.includes("1")) {
-    return "<em>Boop!</em>";
-  } else if (input.includes("0")) {
-    return "<em>Beep!</em>";
-  } else {
-    number = parseInt(input)
-    return number;
+  numbers = []
+  for (input; input >= 0; input--) {
+    if (input % 3 === 0 && input != 0) {
+      console.log("input % 3")
+      numbers.push("<br><em>I'm sorry, Dave. I'm afraid I can't do that.</em>");
+    } else if ((input.toString()).includes("1")) {
+      console.log("includes 1")
+      numbers.push("<br><em>Boop!</em>");
+    } else if ((input.toString()).includes("0")) {
+      console.log("includes 0")
+      numbers.push("<br><em>Beep!</em>");
+    } else {
+      console.log("number: " + input);
+      numbers.push("<br>" + input);
+    }
   }
+  return numbers.join(" ");
 };
 
 //user interface logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var numberInput = $("#numberInput").val();
+    var numberInput = parseInt($("#numberInput").val());
     $(".numberOutput").html(beepFunction(numberInput) + "<br>");
   });
 });
