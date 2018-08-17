66576438486e2a627d23cd12bc2b7d2c98197091
Chris Crow <chrismcrow@gmail.com>
2018-08-17 13:58:06 -0700

    add name feature and reverse feature.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 //business logic
-var name = ""
+var name = "Dave"
 
 function beepFunction(input) {
   numbers = []
   for (i = 0; i <= input; i++) {
     if (i % 3 === 0 && i != 0) {
-      numbers.push("<br><em class='smallText'>I'm sorry, Dave. I'm afraid I can't do that.</em>");
+      numbers.push("<br> <em class='smallText'>I'm sorry, " + name + ". I'm afraid I can't do that.</em>");
     } else if ((i.toString()).includes("1")) {
-      numbers.push("<br><em>Boop!</em>");
+      numbers.push("<br> <em>Boop!</em>");
     } else if ((i.toString()).includes("0")) {
-      numbers.push("<br><em>Beep!</em>");
+      numbers.push("<br> <em>Beep!</em>");
     } else {
-      numbers.push("<br>" + i);
+      numbers.push("<br> " + i);
     }
   }
   return numbers.join(" ");
@@ -21,21 +21,31 @@ function beepFunction(input) {
 $(document).ready(function() {
   $("#enterName").submit(function(event) {
     event.preventDefault();
-    var name = $("#nameInput").val();
-    $("form#enterName").hide();
-    console.log(name);
+    name = $("#nameInput").val();
+    $("form#enterName").slideUp();
     $("form#enterNumber").show();
   });
 
   $("#enterNumber").submit(function(event) {
     event.preventDefault();
     var numberInput = parseInt($("#numberInput").val());
-    $(".numberOutput").html(beepFunction(numberInput));
-    $("div.numberOutput").show();
+    var numberOutput = beepFunction(numberInput);
+    $(".numberOutput").html(numberOutput + "<br>");
+    $("div.numberOutput").slideDown();
+  });
+
+  $("#reverse").click(function(event) {
+    event.preventDefault();
+    var numberInput = parseInt($("#numberInput").val());
+    var numberOutputArray = beepFunction(numberInput).split("<br>");
+    var reverseArray = numberOutputArray.reverse();
+    var reverseOuput = reverseArray.join("<br>");
+    $(".numberOutput").html("<br>" + reverseOuput);
+    $("div.numberOutput").slideDown();
   });
 
   $("button#clear").click(function(event) {
     event.preventDefault();
-    $("div.numberOutput").hide();
+    $("div.numberOutput").slideUp();
   });
 });
