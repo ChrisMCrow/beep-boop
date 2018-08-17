e508e5db3f2d7c613e8a702f9d38feb6da47eb9e
Chris Crow <chrismcrow@gmail.com>
2018-08-17 10:53:34 -0700

    refactor output to count up from 0 instead of down from inputted number in order to better comply with the instructions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
 //business logic
 function beepFunction(input) {
   numbers = []
-  for (input; input >= 0; input--) {
-    if (input % 3 === 0 && input != 0) {
-      console.log("input % 3")
-      numbers.push("<br><em>I'm sorry, Dave. I'm afraid I can't do that.</em>");
-    } else if ((input.toString()).includes("1")) {
-      console.log("includes 1")
+  for (i = 0; i <= input; i++) {
+    if (i % 3 === 0 && i != 0) {
+      numbers.push("<br><em class='smallText'>I'm sorry, Dave. I'm afraid I can't do that.</em>");
+    } else if ((i.toString()).includes("1")) {
       numbers.push("<br><em>Boop!</em>");
-    } else if ((input.toString()).includes("0")) {
-      console.log("includes 0")
+    } else if ((i.toString()).includes("0")) {
       numbers.push("<br><em>Beep!</em>");
     } else {
-      console.log("number: " + input);
-      numbers.push("<br>" + input);
+      numbers.push("<br>" + i);
     }
   }
   return numbers.join(" ");
@@ -24,6 +20,6 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var numberInput = parseInt($("#numberInput").val());
-    $(".numberOutput").html(beepFunction(numberInput) + "<br>");
+    $(".numberOutput").html(beepFunction(numberInput));
   });
 });
